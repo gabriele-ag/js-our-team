0b90f87428b77a7905a49571450c9fe1a0bd235d
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-06 17:57:08 +0200

    feat: creating form

--- js/script.js
+++ js/script.js
@@ -37,39 +37,58 @@ const teamMembers = [
     }
   ];
 
-
-
-const cardElement = document.querySelector(`#card-container`)
+  const cardElement = document.querySelector(`#card-container`)
 
 
 const getElementMember = (member) => {
     const {name, role, email, img} = member
     
-    return `<div class="card mb-3 col-4" style="max-width: 540px;">
-                                    <div class="row g-0">
-                                        <div class="col-md-4">
-                                            <img src="${img}" class="img-fluid rounded-start" alt="">
-                                        </div>
-                                        <div class="col-md-8">
-                                            <div class="card-body">
-                                            <h5 class="card-title">${name}</h5>
-                                            <p class="card-text">${role}</p>
-                                            <p class="card-text">${email}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>`;
+    card = `<div class="card mb-3 col-4" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${img}" class="img-fluid rounded-start" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">${name}</h5>
+                        <p class="card-text">${role}</p>
+                        <p class="card-text">${email}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+
+    return card;
 }
 
+
+// of per gli array
+//  in per gli oggetti
 
 for (let member of teamMembers) {
-    cardElement.innerHTML += getElementMember(member)
+    cardElement.innerHTML += getElementMember(member);
 }
 
+const nameUser = document.querySelector(`#name`)
+const roleUser = document.querySelector(`#role`)
+const mailUser = document.querySelector(`#mail`)
+const imageUser = document.querySelector(`#image`)
+const btn = document.querySelector(`.btn`)
+
+btn.addEventListener(`submit`, function (event) {
+    event.preventDefault()
+
+    const nuovoUtente = {
+        name: nameUser.input,
+        role: roleUser.input,
+        mail: mailUser.input,
+        image: imageUser.input
+    }
+    
+}) 
 
 
 
 
-// for (let member of teamMembers) {
-//     cardElement.innerHTML += getElementMember(teamMembers);
-//   }
+
+
