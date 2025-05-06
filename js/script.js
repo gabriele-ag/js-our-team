fd5bc1da143b2fbaef4635f323b23cadb9493446
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-06 15:18:45 +0200

    feat: create getElementMember function

--- js/script.js
+++ js/script.js
@@ -37,4 +37,28 @@ const teamMembers = [
     }
   ];
 
-  console.log(teamMembers)
+
+
+const card = document.querySelector(`.card`)
+
+  const getElementMember = (elem) => {
+    const {name, role, email, img} = elem
+
+
+    card.innerHTML = `<div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${img}" class="img-fluid rounded-start" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">${name}</h5>
+                        <p class="card-text">${role}</p>
+                        <p class="card-text">${email}</p>
+                        </div>
+                    </div>
+                </div>`
+    return card;
+}
+
+console.log(getElementMember(teamMembers[0]))
+
